f8e9407dbe3a6f4cd4ce477db9e8128e5a9bc506
James Larivee <mr_15th_entertainer@yahoo.com>
2019-09-30 14:34:49 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/cards/model/Card.java
@@ -0,0 +1,37 @@
+package edu.cnm.deepdive.cards.model;
+
+import java.util.Comparator;
+import jdk.internal.dynalink.linker.ConversionComparator.Comparison;
+
+public class Card implements Comparable<Card> {
+
+  private final Rank rank;
+  private final Suit suit;
+
+  public Card(Rank rank, Suit suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+  public Rank getRank() {
+    return rank;
+  }
+
+  public Suit getSuit() {
+    return suit;
+  }
+
+  @Override
+  public int compareTo(Card other) {
+    int comparison = suit.compareTo(other.suit);
+    if(comparison == 0) {
+      comparison = rank.compareTo(other.rank);
+    }
+    return comparison;
+  }
+
+  @Override
+  public String toString() {
+    return rank.getSymbol() + suit.getSymbol();
+  }
+}
